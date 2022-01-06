b7e075f3d3fe9ef3d8f88836041a1e0021439d65
Gabe Dunn <gabed@hey.com>
2022-01-05 21:09:04 -0700

    add F13 key & replace KC_NO with _______

--- keyboards/gmmk/pro/ansi/keymaps/redxtech/keymap.c
+++ keyboards/gmmk/pro/ansi/keymaps/redxtech/keymap.c
@@ -13,7 +13,6 @@ enum custom_keycodes {
 #define _RL 3 // RGB layer
 #define _MS 4 // mouse layer
 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //      ESC         F1      F2      F3      F4      F5      F6      F7      F8      F9      F10         F11         F12         Prt                 Rotary(Play/Pause)
     //      ~           1       2       3       4       5       6       7       8       9       0           -           (=)         BackSpc             Del (PGUP)
@@ -25,33 +24,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_ESC,     KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10,     KC_F11,     KC_F12,     KC_PSCR,            KC_MPLY,
             KC_GRV,     KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,            KC_PGUP,
             KC_TAB,     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,            KC_PGDN,
-            HYPR(KC_NO),KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,    KC_QUOT,                KC_ENT,             KC_END,
+            KC_F13,     KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,    KC_QUOT,                KC_ENT,             KC_END,
             KC_LSFT,    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,                            KC_RSFT,    KC_UP,  KC_DEL,
-            KC_LCTL,    KC_LGUI,KC_LALT,                        KC_SPC,                         KC_RALT,    MO(_FL),    KC_RCTL,    KC_LEFT,    KC_DOWN, KC_RGHT
+            KC_LCTL,    KC_LGUI,KC_LALT,                        KC_SPC,                         KC_RALT,    MO(_FL),    KC_RCTL,    KC_LEFT,    KC_DOWN,KC_RGHT
     ),
 	[_FL] = LAYOUT(
-            EEP_RST,    _______,_______,_______,_______,KC_SLEP,KC_PWR, KC_VOLD,KC_VOLU,KC_MSTP,KC_MPLY,    KC_MPRV,    KC_MNXT,    KC_SLCK,            KC_MUTE,
+            EEP_RST,    KC_F13, KC_F14, KC_F15, KC_F16, KC_SLEP,KC_PWR, KC_VOLD,KC_VOLU,KC_MSTP,KC_MPLY,    KC_MPRV,    KC_MNXT,    KC_SLCK,            KC_MUTE,
             _______,    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,    _______,    _______,    _______,            _______,
             _______,    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,    _______,    _______,    RESET,              _______,
             KC_CAPS,    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,    _______,                _______,            KC_HOME,
             _______,    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,                            _______,    _______,KC_INS,
-            _______,    _______,_______,                        _______,                        MO(2),      _______,    MO(3),      _______,    _______, _______
+            _______,    _______,_______,                        _______,                        MO(2),      _______,    MO(3),      _______,    _______,_______
     ),
 	[_ML] = LAYOUT(
-            KC_NO,      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_NO,      KC_NO,      KC_NO,              KC_NO,
-            TG(4),      TG(0),  TG(5),  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_NO,      KC_NO,      KC_NO,              KC_NO,
-            KC_NO,      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_NO,      KC_NO,      KC_NO,              MACRO_BOOT_WIN,
-            KC_NO,      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_NO,                  KC_NO,              MACRO_SEC_BOOT,
-            KC_NO,      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,                              KC_NO,      KC_NO,  KC_DEL_SPAM,
-            KC_NO,      KC_NO,  KC_NO,                          KC_NO,                          _______,    _______,    KC_NO,      KC_NO,      KC_NO,  KC_NO
+            _______,    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,    _______,    _______,    _______,            _______,
+            TG(4),      TG(0),  TG(5),  _______,_______,_______,_______,_______,_______,_______,_______,    _______,    _______,    _______,            _______,
+            _______,    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,    _______,    _______,    _______,            MACRO_BOOT_WIN,
+            _______,    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,    _______,                _______,            MACRO_SEC_BOOT,
+            _______,    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,                            _______,    _______,KC_DEL_SPAM,
+            _______,    _______,_______,                        _______,                        _______,    _______,    _______,    _______,    _______,_______
     ),
 	[_RL] = LAYOUT(
-            EEP_RST,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_NO,      KC_NO,      KC_NO,              KC_NO,
-            KC_NO,      RGB_TOG,RGB_M_P,RGB_M_B,RGB_M_R,RGB_M_SW,RGB_M_SN,RGB_M_K,RGB_M_X,RGB_M_G,RGB_M_T,  KC_NO,      KC_NO,      KC_NO,              KC_NO,
-            KC_NO,      BL_ON,  BL_INC, RGB_VAI,RGB_HUI,RGB_SAI,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_NO,      KC_NO,      KC_NO,              KC_NO,
-            KC_NO,      BL_DEC, BL_DEC, RGB_VAD,RGB_HUD,RGB_SAD,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_NO,                  KC_NO,              KC_NO,
-            KC_NO,      BL_TOGG,BL_BRTG,BL_STEP,KC_NO,  KC_NO,  NK_TOGG,KC_NO,  KC_NO,  KC_NO,  KC_NO,                              KC_NO,      RGB_MOD,KC_NO,
-            KC_NO,      KC_NO,  KC_NO,                          KC_NO,                          KC_NO,      _______,    _______,    RGB_SPD,    RGB_RMOD,RGB_SPI
+            EEP_RST,    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,    _______,    _______,    _______,            _______,
+            _______,    RGB_TOG,RGB_M_P,RGB_M_B,RGB_M_R,RGB_M_SW,RGB_M_SN,RGB_M_K,RGB_M_X,RGB_M_G,RGB_M_T,  _______,    _______,    _______,            _______,
+            _______,    BL_ON,  BL_INC, RGB_VAI,RGB_HUI,RGB_SAI,_______,_______,_______,_______,_______,    _______,    _______,    _______,            _______,
+            _______,    BL_DEC, BL_DEC, RGB_VAD,RGB_HUD,RGB_SAD,_______,_______,_______,_______,_______,    _______,                _______,            _______,
+            _______,    BL_TOGG,BL_BRTG,BL_STEP,_______,_______,NK_TOGG,_______,_______,_______,_______,                            _______,    RGB_MOD,_______,
+            _______,    _______,_______,                        _______,                        _______,    _______,    _______,    RGB_SPD,    RGB_RMOD,RGB_SPI
     ),
 	[_MS] = LAYOUT(
             _______,    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,    _______,    _______,    _______,            _______,
